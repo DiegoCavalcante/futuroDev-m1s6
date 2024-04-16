6cc9a3194986be97d57f35476577f3950d0f3498
cavalcante.diego <diegosc.cavalcante@gmail.com>
2024-04-16 00:16:18 -0300

    Mudança de nome e login via js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function saudacao(){
 }
 
 saudacao()
+
+const nome = document.getElementById('nome')
+const login = document.getElementById('login')
+nome.innerHTML = 'Diego Cavalcante'
+login.innerHTML = 'diegosc.cavalcante'
